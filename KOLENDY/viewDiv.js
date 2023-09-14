ef564d2bab997a5f90a3804b87534bcbcc1f2bfa
Rozum <80721089+mentis-RD@users.noreply.github.com>
2023-09-14 18:20:51 +0200

    Update viewDiv.js

--- KOLENDY/viewDiv.js
+++ KOLENDY/viewDiv.js
@@ -4,8 +4,9 @@ buttons.forEach(button => {
     button.addEventListener("click", () => {
         const div = document.querySelector(`#div${button.dataset.id}`);
         const pastDiv = document.querySelector(".show");
-        pastDiv.classList.remove("show");
+        if (pastDiv) {
+            pastDiv.classList.remove("show");
+        }
         div.classList.add("show");
-
     });
 });
